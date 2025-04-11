01803b42ada8cc43949e474b61da7226215ec8d5
S-a-m-93 <kusalasameera.g22@iiits.in>
2025-04-11 22:17:21 +0530

    [add] splice site prediction frontend + integration

---
+++ src/app/(models_interaction)/spliceSitePrediction/page.tsx
@@ -0,0 +1,8 @@
+import { SpliceSitePrediction } from "@/components/spliceSitePredictionPage/SpliceSitePrediction";
+export default function SarsClassificationMutationsPage() {
+    return (
+      <div className="flex flex-col w-full items-center justify-center">
+        <SpliceSitePrediction/>
+      </div>
+    );
+  }
---
+++ src/lib/store/useSpliceStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+
+interface SpliceState {
+  results: any;
+  modelType: "donor" | "acceptor";
+  sequenceInput: string;
+  files: File[];
+  isSampleFileUsed: boolean;
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+  setResults: (results: any) => void;
+  setModelType: (type: "donor" | "acceptor") => void;
+  setSequenceInput: (input: string) => void;
+  setFiles: (files: File[]) => void;
+  setIsSampleFileUsed: (used: boolean) => void;
+  resetState: () => void;
+}
+
+export const useSpliceStore = create<SpliceState>((set) => ({
+  results: null,
+  modelType: "acceptor",
+  sequenceInput: "",
+  files: [],
+  isSampleFileUsed: false,
+  setResults: (results) => set({ results }),
+  setModelType: (type) => set({ modelType: type }),
+  setSequenceInput: (input) => set({ sequenceInput: input }),
+  setFiles: (files) => set({ files }),
+  activeTab: 'analysis',
+  setActiveTab: (tab) => set({ activeTab: tab }),
+  setIsSampleFileUsed: (used) => set({ isSampleFileUsed: used }),
+  resetState: () => set({
+    results: null,
+    modelType: "acceptor",
+    sequenceInput: "",
+    files: [],
+    isSampleFileUsed: false,
+  }),
+}));
